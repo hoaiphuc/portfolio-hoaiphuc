13fcb9d000f6abb74c4ac08cb2b1b911c7fbb379
hoaiphuc <nguyenhoaiphuc3122001@gmail.com>
2024-08-15 18:18:09 +0700

    fix deploy

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const port = process.env.PORT || 5000;
 const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
 app.listen(port, () => {
