82864361837213fafdd70414d996935a5899bb86
hoaiphuc <nguyenhoaiphuc3122001@gmail.com>
2024-08-15 20:16:11 +0700

    fix smth

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,14 @@ app.use(express.json());
 app.use("/api/portfolio", portfolioRoute);
 
 const port = process.env.PORT || 5000;
-const path = require("path");
+// const path = require("path");
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client/build/index.html"));
+//   });
+// }
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
